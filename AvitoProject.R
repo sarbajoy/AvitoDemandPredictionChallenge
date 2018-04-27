2952bea0452114ba87669502dad6fa7eb22e98fd
Sarbajoy Majumdar <sarbajoymajumdar@gmail.com>
2018-04-27 14:15:37 +0800

    Update code
    Removed translation for loop

--- AvitoProject.R
+++ AvitoProject.R
@@ -1,54 +1,37 @@
 rm(list=ls())
 
-avitoTrain = read.csv("Downloads/AvitoTrain.csv")
+Avito = read.csv("Downloads/AvitoTrain.csv")
 
 ##set up translate
 library(translate)
 set.key("AIzaSyAJvkZgYIjwNDTiz6pAWZOV-bS4oveRd_4")
 
+##translate(region,"ru","en")
+
 ######################### CLEAN UP DATA #######################################
 
 ##delete columns that do not play a role
-AvitoTrain$item_id=NULL
-AvitoTrain$user_id=NULL
-AvitoTrain$image=NULL
+Avito$item_id=NULL
+Avito$user_id=NULL
+Avito$image=NULL
 
 ##create new columns to separate user type
-AvitoTrain$IsPrivate <- ifelse(AvitoTrain$user_type == 'Private',c(1), c(0))
-AvitoTrain$IsCompany <- ifelse(AvitoTrain$user_type == 'Company',c(1), c(0))
-AvitoTrain$user_type = NULL
+Avito$IsPrivate <- ifelse(Avito$user_type == 'Private',c(1), c(0))
+Avito$IsCompany <- ifelse(Avito$user_type == 'Company',c(1), c(0))
+Avito$user_type = NULL
 
 ##find length of title and description
-AvitoTrain$titleLength <- nchar(AvitoTrain$title,allowNA = TRUE, keepNA = 0)
-AvitoTrain$descriptionLength <- ifelse (is.na(AvitoTrain$description),
-c(0), c(nchar(AvitoTrain$description)))
+Avito$titleLength <- nchar(Avito$title,allowNA = TRUE, keepNA = 0)
+Avito$descriptionLength <- ifelse (is.na(Avito$description),
+c(0), c(nchar(Avito$description)))
 
-##create new columns for translations
-##they will most likely give default values of first row
-##next step is to clean up and put actual translations
-AvitoTrain$Region <- translate(AvitoTrain$region,"ru","en")
-AvitoTrain$City <- translate(AvitoTrain$city,"ru","en")
-AvitoTrain$Parent <- translate(AvitoTrain$parent_category_name,"ru","en")
-AvitoTrain$Category <- translate(AvitoTrain$category_name,"ru","en")
 
-for (i in 2:nrow(AvitoTrain)){
-  #retrieve all variables needed for translation
-  region = AvitoTrain [i,1]
-  city = AvitoTrain [i,2]
-  parent = AvitoTrain [i,3]
-  category = AvitoTrain [i,4]
-
-  AvitoTrain[i,19]=translate(region,"ru","en")
-  AvitoTrain [i,20] = translate (city,"ru","en")
-  AvitoTrain [i,21] = translate (parent,"ru","en")
-  AvitoTrain [i,22] = translate (category,"ru","en")
-}
 
 #drop columns
-AvitoTrain$region = NULL
-AvitoTrain$city = NULL
-AvitoTrain$parent_category_name = NULL
-AvitoTrain$category_name = NULL
+Avito$region = NULL
+Avito$city = NULL
+Avito$parent_category_name = NULL
+Avito$category_name = NULL
 
 ## count number of parameters
 
@@ -60,7 +43,8 @@ AvitoTrain$category_name = NULL
 
 ##find unique cities and make bin variables
 
-##split data
+##split data into training, testing and validation
+set.seed(1951)
 
 #################### MAKE PREDICTIVE MODELS ###################################
 
